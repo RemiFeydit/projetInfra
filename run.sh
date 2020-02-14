681a2f0633987cde3523201e9cf746880c0e53a7
RemiFeydit <remi.feydit@ynov.com>
2020-02-14 17:15:57 +0100

    début page web

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ apt-get install default-jdk -y
 apt-get install apache2 -y
 ufw allow 'Apache'
 mkdir /var/www/minecraft
-cd /var/www/minecraft
+mkdir /var/www/minecraft/minecraftServer
+cd /var/www/minecraft/minecraftServer
 wget https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar -O /var/www/minecraft/minecraft_server1_15.jar
 chmod +x minecraft_server1_15.jar
 ufw allow 25565
