929c9a4de05d281ca6836ad2f88639e0f1eb84bc
Mathieu <mathieu.caselles@ynov.com>
2020-02-21 15:48:31 +0100

    ajout autorisation script .sh

--- run.sh
+++ run.sh
@@ -8,6 +8,13 @@ mkdir /var/www/minecraft/minecraftServer
 mkdir /var/www/minecraft/backupMinecraft/backup
 mv commandes /var/www/minecraft
 
+cd /var/www/minecraft/commandes
+
+chmod +xrw ./backup.sh
+chmod +xrw ./changeProperties.sh
+chmod +xrw ./changeVersion.sh
+chmod +x ./start.sh
+
 cd /var/www/minecraft/minecraftServer
 wget https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar -O /var/www/minecraft/minecraft_server1_15.jar
 chmod +x minecraft_server1_15.jar
