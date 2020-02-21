f4410b977b33dfa53e854a7ebeb6b071a86976f4
MathieuCaselles <mathieu.caselles@ynov.com>
2020-02-21 14:51:41 +0100

    maj gestion eula.txt

--- run.sh
+++ run.sh
@@ -8,6 +8,5 @@ cd /var/www/minecraft/minecraftServer
 wget https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar -O /var/www/minecraft/minecraft_server1_15.jar
 chmod +x minecraft_server1_15.jar
 ufw allow 25565
-java -Xmx1024M -Xms1024M -jar minecraft_server1_15.jar -nogui
-sed -i 's/false/true/g' eula.txt
+echo 'eula=true'>eula.txt
 java -Xmx1024M -Xms1024M -jar minecraft_server1_15.jar -nogui
