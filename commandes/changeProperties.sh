58da332c5e772c3e996ed7c5d84e821e6105225f
MathieuCaselles <mathieu.caselles@ynov.com>
2020-02-22 21:12:51 +0100

    Update changeProperties.sh

--- commandes/changeProperties.sh
+++ commandes/changeProperties.sh
@@ -3,7 +3,7 @@
 propriete=$1
 modification=$2
 
-
+cd ../minecraftServer
 valeurActuelle=$(grep $propriete ./server.properties | cut -d'=' -f2)
 sed -i 's/'$propriete'='$valeurActuelle'/'$propriete'='$modification'/g' server.properties
 echo 'Modification terminé !'
