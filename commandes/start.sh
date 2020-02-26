d48c3748fe2fa7b01f53e1322eba091d6902be84
MathieuCaselles <mathieu.caselles@ynov.com>
2020-02-26 03:44:17 +0100

    Update start.sh

--- commandes/start.sh
+++ commandes/start.sh
@@ -2,4 +2,5 @@ cd /var/www/minecraft/minecraftServer/
 sed -i 's/false/true/g' eula.txt
 sed -i 's/enable-rcon=false/enable-rcon=true/g' server.properties
 sed -i 's/rcon.password=/rcon.password=azerty/g' server.properties
+sed -i 's/rcon.password=azertyazerty/rcon.password=azerty/g' server.properties
 java -Xmx1024M -Xms1024M -jar server.jar -nogui > log.txt &
