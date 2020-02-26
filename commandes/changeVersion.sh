06e662652d66ce70659eb3168f23a3c03761d2fb
MathieuCaselles <mathieu.caselles@ynov.com>
2020-02-26 03:19:10 +0100

    Update changeVersion.sh

--- commandes/changeVersion.sh
+++ commandes/changeVersion.sh
@@ -6,22 +6,19 @@ save=$2
 function start {
     version=$1
 
-    lienVersion=$(grep $version listeVersion.txt | cut -d'=' -f2)
+    lienVersion=$(grep $version /var/www/minecraft/commandes/listeVersion.txt | cut -d'=' -f2)
 
     if ! test -z $lienVersion
         then
-
-        echo Voulez vous sauvegarder le monde et les propriétés de la version actuelle ?
-        echo Vous pourrez tout récupérer en revenant à cette version.
 
         if test $save == "true"
             then
             ./backup.sh save all
         fi
 
-        rm -rf ../minecraftServer
-        mkdir ../minecraftServer
-        cd ../minecraftServer
+        rm -rf /var/www/minecraft/minecraftServer
+        mkdir /var/www/minecraft/minecraftServer
+        cd /var/www/minecraft//minecraftServer
         wget $lienVersion
         echo $version>versionActuelle.txt
 	java -Xmx1024M -Xms1024M -jar server.jar -nogui
